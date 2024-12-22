7b1ffc355f3b9dd1cdaad8b08deec69136c0f2d7
Rafal Karpicki <rafal.karpicki@syzygy.pl>
2024-12-22 15:35:51 +0100

    Added ngrx stores

--- projects/job-offer-ngrx/src/app/app.config.ts
+++ projects/job-offer-ngrx/src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import {provideRouterStore, routerReducer} from '@ngrx/router-store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideStore({
+    router: routerReducer,
+  }), provideEffects(), provideRouterStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
+
